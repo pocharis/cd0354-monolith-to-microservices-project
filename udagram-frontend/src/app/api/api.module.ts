508e06f9477564489d5fadc05bf54bec8b1e5814
pocha <arinze6@gmail.com>
2022-07-07 11:40:56 -0700

    travis deployment

--- udagram-frontend/src/app/api/api.module.ts
+++ udagram-frontend/src/app/api/api.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 const components = [];
 
 @NgModule({
-  imports: [
-    HttpClientModule,
-  ],
+  imports: [HttpClientModule],
   declarations: components,
   exports: components,
-  providers: []
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ApiModule {}
